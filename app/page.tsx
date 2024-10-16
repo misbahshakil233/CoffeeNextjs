d3292d8668a78c23422101273915826653134665
misbahshakil233 <141862743+misbahshakil233@users.noreply.github.com>
2024-10-16 23:00:52 +0500

    all bugs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import About from "@/components/About";
 import ClientReviews from "@/components/ClientReviews";
 import CoffeeSlider from "@/components/CoffeeSlider";
 import Contact from "@/components/Contact";
-import DiscountOffer from "@/components/DiscountOffer";
+
 
 import Hero from "@/components/Hero";
 import InfoBanner from "@/components/InfoBanner";
 
 import Product from "@/components/Product";
-import Service from "@/components/Service";
+
 
 
 
@@ -22,8 +22,7 @@ export default function Home() {
        <CoffeeSlider />
       <Product />
       <ClientReviews />
-      {/* <Service /> */}
-      {/* <DiscountOffer /> */}
+    
       <Contact />   
     </div>
   );
